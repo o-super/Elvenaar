e099663202fcd7933d38993a7140c8abfa2dbdda
Olivier Barraud <barraudolivier@gmail.com>
2022-01-29 19:58:45 -0800

    Divided jump by two

--- Data/Subfolders/Spellshock Weapons/Scripts/ImpulseBoostServer.lua
+++ Data/Subfolders/Spellshock Weapons/Scripts/ImpulseBoostServer.lua
@@ -6,7 +6,7 @@ function Boost(ability)
     local fwdVector = ability.owner:GetWorldTransform():GetForwardVector() * BOOST_DIRECTION.x
     local rightVector = ability.owner:GetWorldTransform():GetRightVector() * BOOST_DIRECTION.y
     local upVector = ability.owner:GetWorldTransform():GetUpVector() * BOOST_DIRECTION.z
-    ability.owner:AddImpulse((fwdVector + rightVector + upVector) * ability.owner.mass)
+    ability.owner:AddImpulse((fwdVector + rightVector + upVector) * (ability.owner.mass / 2))
 end
 
 ABILITY.castEvent:Connect(Boost)
