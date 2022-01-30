bb2b23de84753df13ecd1e1f852eecfd1f60f8e8
Olivier Barraud <barraudolivier@gmail.com>
2022-01-29 16:23:01 -0800

    Improved invisible wall not blocking the players properly

--- Data/Scripts/2Frogs-InvisibleWall.lua
+++ Data/Scripts/2Frogs-InvisibleWall.lua
@@ -3,19 +3,22 @@ local PREVENT_PROJECTILES = script:GetCustomProperty("PreventProjectiles")
 local trigger = script.parent
 
 function OnBeginOverlap(theTrigger, player)
-    if player ~= nil and player:IsA("player") then
+    if player ~= nil and player:IsA("Player") then
+        player.movementControlMode = MovementControlMode.NONE
         player:AddImpulse(player:GetWorldTransform():GetForwardVector() * player.mass * 1000 * -1)
     else
         if PREVENT_PROJECTILES == true and Object.IsValid(player) and player:IsA("Projectile") and player.owner.team ~= trigger.team then
-            print("Proc: " .. tostring(player))
             player:Destroy()
         end
     end
 end
 
 function OnEndOverlap(theTrigger, player)
+    if player ~= nil and player:IsA("Player") then
+        player.movementControlMode = MovementControlMode.VIEW_RELATIVE
+    end
 end
 
 
 trigger.beginOverlapEvent:Connect(OnBeginOverlap)
---trigger.endOverlapEvent:Connect(OnEndOverlap)
+trigger.endOverlapEvent:Connect(OnEndOverlap)
